9f85a614ceba7dd31025dc5d818a27052b8a816f
Tosin <tosinmoses384@gmail.com>
2019-12-19 12:08:12 +0100

    AuthUsersPermissionPart2

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 // @desc        Register user
@@ -47,30 +48,6 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
-// Get token from model, create cookie and send response
-const sendTokenResponse = (user, statusCode, res) => {
-   // Create token
-   const token = user.getSignedJwtToken();
-
-   const options = {
-     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-     httpOnly: true
-   };
-
-   if (process.env.NODE_ENV === 'production') {
-      options.secure = true;     
-   }
-
-   res
-    .status(statusCode)
-    .cookie('token', token, options)
-    .json({
-      success: true,
-      token
-    });
-
-};
-
 // @desc        Get current logged in user
 // @route       POST /api/v1/auth/me
 // @access      Private
@@ -82,3 +59,73 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     data: user
   });
 });
+
+// @desc        Forgot password
+// @route       POST /api/v1/auth/forgotpassword
+// @access      Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has
+  requested the reset of a password.  Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message
+    });
+
+    res.status(200).json({ success: true, data: 'Email sent' });
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+     options.secure = true;     
+  }
+
+  res
+   .status(statusCode)
+   .cookie('token', token, options)
+   .json({
+     success: true,
+     token
+   });
+
+};
--- controllers/courses.js
+++ controllers/courses.js
@@ -8,7 +8,7 @@ const Bootcamp = require('../models/Bootcamp');
 // @route       GET /api/v1/bootcamps/:bootcampId/courses
 // @access      Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-    if (req.params.bootcampId) {
+  if (req.params.bootcampId) {
     const courses = await Course.find({ bootcamp: req.params.bootcampId });
 
     return res.status(200).json({
@@ -48,6 +48,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access      Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
 
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
@@ -55,6 +56,16 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     return next(
       new ErrorResponse(`No bootcamp with the id of ${req.param.bootcampId}`),
       404
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
+        401
+      )
     );
   }
 
@@ -70,44 +81,64 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 // @route       PUT /api/v1/courses/:id
 // @access      Private
 exports.updateCourse = asyncHandler(async (req, res, next) => {
-    let course = await Course.findById(req.params.id);
-  
-    if (!course) {
-      return next(
-        new ErrorResponse(`No course with the id of ${req.param.id}`),
-        404
-      );
-    }
-  
-    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
-  
-    res.status(200).json({
-      success: true,
-      data: course
-    });
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.param.id}`),
+      404
+    );
+  }
+
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update course ${course._id}`,
+        401
+      )
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
   });
+
+  res.status(200).json({
+    success: true,
+    data: course
+  });
+});
 
 // @desc        Delete course
 // @route       DELETE /api/v1/courses/:id
 // @access      Private
 exports.deleteCourse = asyncHandler(async (req, res, next) => {
-    const course = await Course.findById(req.params.id);
-  
-    if (!course) {
-      return next(
-        new ErrorResponse(`No course with the id of ${req.param.id}`),
-        404
-      );
-    }
-  
-    await course.remove();
-  
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  });
+  const course = await Course.findById(req.params.id);
 
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.param.id}`),
+      404
+    );
+  }
+
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete course ${course._id}`,
+        401
+      )
+    );
+  }
+
+
+  await course.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
