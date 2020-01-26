8b8ed22d850be57e0512d655773a3329e184b8ef
Tosin <tosinmoses384@gmail.com>
2020-01-26 12:15:05 +0100

    Finished bootcampReviews&Ratings

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
 const users = require('./routes/users');
+const reviews = require('./routes/reviews')
 
 const app = express();
 
@@ -40,10 +41,11 @@ app.use(fileupload());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
-app.use('/api/V1/bootcamps', bootcamps);
-app.use('/api/V1/courses', courses);
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
 
 app.use(errorHandler);
 
